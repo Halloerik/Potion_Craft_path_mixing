0cb7ed96c82c335c6f4520adca07dab0fae38df2
Halloerik <Halloerik95@gmail.com>
2021-11-08 12:30:48 +0100

    Initial Commit

---
+++ bezier.py
@@ -0,0 +1,107 @@
+import numpy as np
+
+
+class CubicBezierCurve:
+    def __init__(self, control_points, line_segments=100):
+        self.control_points = control_points
+
+        self.line_segments = line_segments
+        self.coordinates = self.bernstein_cubic_line()
+        self.look_up_table = []
+        self.generate_look_up_table()
+        self.length = self.look_up_table[-1][1]
+
+    def bernstein_cubic_point(self, t):
+
+        p0 = self.control_points[0] * (- t ** 3 + 3 * t ** 2 - 3 * t + 1)
+        p1 = self.control_points[1] * (3 * t ** 3 - 6 * t ** 2 + 3 * t)
+        p2 = self.control_points[2] * (-3 * t ** 3 + 3 * t ** 2)
+        p3 = self.control_points[3] * (t ** 3)
+        return p0 + p1 + p2 + p3
+
+    def speed(self, t):
+        p0 = self.control_points[0] * (- 3 * t ** 2 + 6 * t - 3)
+        p1 = self.control_points[1] * (9 * t ** 2 - 12 * t + 3)
+        p2 = self.control_points[2] * (-9 * t ** 2 + 6 * t)
+        p3 = self.control_points[3] * (3 * t ** 2)
+        return p0 + p1 + p2 + p3
+
+    def acceleration(self, t):
+        p0 = self.control_points[0] * (- 6 * t + 6)
+        p1 = self.control_points[1] * (18 * t - 12)
+        p2 = self.control_points[2] * (-18 * t + 6)
+        p3 = self.control_points[3] * (6 * t)
+        return p0 + p1 + p2 + p3
+
+    def bernstein_cubic_line(self):
+        points = []
+        for t in range(self.line_segments + 1):
+            points.append(self.bernstein_cubic_point(t / self.line_segments))
+
+        return np.array(points)
+
+    def generate_look_up_table(self):
+        self.look_up_table = [[0, 0]]
+        previous_point = self.bernstein_cubic_point(0)
+        for t in range(1, self.line_segments + 1):
+            current_point = self.bernstein_cubic_point(t / (self.line_segments))
+            distance = np.linalg.norm(current_point - previous_point)
+            self.look_up_table.append([t/ (self.line_segments), distance])
+
+
+    def get_distance(self, t):
+        for i in range(1, self.line_segments + 1):
+            t1, d1 = self.look_up_table[i - 1]
+            t2, d2 = self.look_up_table[i]
+            if t1 <= t <= t2:
+                t = (t - t1) / (t2 - t1)
+                return d1 * t + d2 * (1 - t)
+
+    def get_t(self, d):
+        for i in range(1, self.line_segments + 1):
+            t1, d1 = self.look_up_table[i - 1]
+            t2, d2 = self.look_up_table[i]
+            if d1 <= d <= d2:
+                d = (d - d1) / (d2 - d1)
+                return t1 * d + t2 * (1 - d)
+
+
+
+class BezierCurve:
+    def __init__(self, curves, line_segments=100):
+        self.curves = [CubicBezierCurve(curve, line_segments) for curve in curves]
+
+        self.line_segments = line_segments
+        self.coordinates = self.curves[0].coordinates
+        self.length = self.curves[0].length
+        for curve in self.curves[1:]:
+            self.coordinates = np.vstack((self.coordinates, curve.coordinates))
+            self.length += curve.length
+
+    def get_t(self, d):
+        for curve in self.curves:
+            #print("d:",d, "curve length:", curve.length)
+            if d - curve.length <= 0:
+                t = curve.get_t(d)
+                #print("t",t,"\n")
+                return curve, t
+            else:
+                d -= curve.length
+        return self.curves[-1], 1
+
+    def get_d_percentage(self, p):
+        return p * self.length  
+
+    def get_coordinate(self, t):
+        d = self.get_d_percentage(t)
+        #print(d)
+        curve, t = self.get_t(d)
+        return curve.bernstein_cubic_point(t)
+
+
+def add_curves(curves: BezierCurve, segments=100):
+    coordinates = np.zeros((segments + 1, 2))
+    for i in range(1, segments + 1):
+        sum_ = sum(curve.get_coordinate(i / (segments)) for curve in curves)
+        coordinates[i] = sum_ #/ len(curves)
+    return coordinates
---
+++ main.py
@@ -0,0 +1,97 @@
+import pyqtgraph as pg
+import numpy as np
+import bezier
+from ingredient import *
+import random
+from PyQt5 import QtGui, QtCore, QtWidgets
+import sys
+
+pg.setConfigOption('background', 'w')
+pg.setConfigOption('foreground', 'k')
+
+mixed_path_segments = 1000
+
+def plot_ingredients(plot_widget, ingredients, colors=None):
+    for i, ingredient in enumerate(ingredients):
+        if colors is None:
+            pen = pg.mkPen((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+        else:
+            pen = pg.mkPen(colors[i])
+
+        coords = bezier.BezierCurve(ingredient).coordinates
+
+        plot = plot_widget.plot(pen=pen)
+        plot.setData(coords)
+
+
+def plot_average_ingredient(plot_widget, ingredients, segments=100, color=None):
+    if color is None:
+        pen = pg.mkPen((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+    else:
+        pen = pg.mkPen(color)
+
+    coords = average_ingredients(ingredients, segments)
+
+    plot = plot_widget.plot(pen=pen)
+    plot.setData(coords)
+
+
+def average_ingredients(ingredients, segments=100):
+    curves = [bezier.BezierCurve(ingredient) for ingredient in ingredients]
+    return bezier.add_curves(curves, segments)
+
+
+selected_ingredients = [waterbloom, terraria]
+
+
+def update_graph(plot_widget, first=None, second=None, segments=50):
+    plot_widget.clear()
+    plot_widget.setXRange(-10, 10)
+    plot_widget.setYRange(-10, 10)
+
+    plot_widget.addItem(pg.InfiniteLine(angle=90))
+    plot_widget.addItem(pg.InfiniteLine(angle=0))
+    if first is not None:
+        selected_ingredients[0] = all_ingredients[first]
+    if second is not None:
+        selected_ingredients[1] = all_ingredients[second]
+
+    plot_ingredients(plot_widget, selected_ingredients)
+    plot_average_ingredient(plot_widget, selected_ingredients, segments)
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+
+    app = pg.mkQApp()
+
+    window = QtWidgets.QWidget()
+    v_box = QtWidgets.QVBoxLayout()
+
+    plot_widget = pg.PlotWidget(None)
+    v_box.addWidget(plot_widget)
+
+    drop_down_1 = QtWidgets.QComboBox()
+    drop_down_1.addItems(all_ingredient_names)
+    drop_down_1.currentIndexChanged.connect(lambda x: update_graph(plot_widget, first=x,segments=mixed_path_segments))
+    v_box.addWidget(drop_down_1)
+
+    drop_down_2 = QtWidgets.QComboBox()
+    drop_down_2.addItems(all_ingredient_names)
+    drop_down_2.setCurrentIndex(1)
+    drop_down_2.currentIndexChanged.connect(lambda x: update_graph(plot_widget, second=x,segments=mixed_path_segments))
+    v_box.addWidget(drop_down_2)
+    window.setLayout(v_box)
+
+    update_graph(plot_widget)
+    # plot_ingredients(plot_widget, all_ingredients)
+
+
+
+    window.show()
+
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        QtGui.QApplication.instance().exec_()
